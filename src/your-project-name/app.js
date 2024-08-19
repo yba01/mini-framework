e2361b7beac3c6a153778836c28d4ee98776de69
adiane <gangalydiane567@gmail.com>
2024-08-19 22:55:32 +0100

    routes handling

--- src/your-project-name/app.js
+++ src/your-project-name/app.js
@@ -1,10 +1,11 @@
 import { Router } from "../core/router.js";
 import HomeComponent from "./components/Home.js";
+import TeamComponent from "./components/Team.js";
 
 
-const router = new Router
+export const router = new Router
 
 router.registerRoute('/', HomeComponent)
+router.registerRoute('/team', TeamComponent)
 
 router.navigateTo('/')
-
